ddb1e9e89d62f501ea866ddfe4f9cc88a2c94345
shablargablargn <shablargablargn@Jonahs-Laptop.localdomain>
2023-08-24 13:09:34 -0500

    index

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.container');
+const freelancers = [
+    { name: "Dr. Slice", price: 25, occupation: "gardener" },
+    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+    { name: "Prof. Prism", price: 81, occupation: "teacher" },
+    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "programmer" },
+    { name: "Dr. Wire", price: 47, occupation: "teacher" },
+    { name: "Prof. Goose", price: 72, occupation: "driver" },
+  ];
+
+
+  
